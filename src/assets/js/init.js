315af6552d9f34c3b6d447ab28bd4bf2d0746b5c
wangjinqing123 <284040231@qq.com>
2017-02-13 11:04:26 +0800

    kong

--- src/assets/js/init.js
+++
@@ -1,7 +0,0 @@
-// 页面滑动流畅
-var init = function () {
-    document.addEventListener('touchmove', function (e) {
-        e.preventDefault();
-    }, false);
-}
-export default init;
--- src/vuex/mutation-types.js
+++
@@ -1,3 +0,0 @@
-// // index.html,获取首页数据
-// export const HOME_GET_LIST = 'HOME_GET_LIST'
-
